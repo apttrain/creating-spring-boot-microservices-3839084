323f99d2fb84f8bcee52443bc88c1bbc7a4bfe76
apttrain <60334261+apttrain@users.noreply.github.com>
2025-02-22 12:32:06 +0000

    adding challenge methods

--- explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourRepository.java
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourRepository.java
@@ -1,8 +1,15 @@
 package com.example.explorecalijpa.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.explorecalijpa.model.Difficulty;
 import com.example.explorecalijpa.model.Tour;
 
 public interface TourRepository extends JpaRepository<Tour, Integer> {
+
+  public List<Tour> findByDifficulty(Difficulty difficulty);
+
+  public List<Tour> findByTourPackageCode(String code);
 }
